5707f1348de54b61bd2c3a9bf2cc7204d1076105
admin <admin@Ice-and-Fire>
2025-08-26 17:24:08 +0530

    3rd Commit. Only Q4 remaining.

--- Day_02/src/day_02/q08_PowerOfANumber.java
+++ Day_02/src/day_02/q08_PowerOfANumber.java
@@ -4,8 +4,9 @@ public class q08_PowerOfANumber {
 	public static void main(String[] args) {
 
 		int base = 3;
-		int power = 4;
+		int power = 1;
 		int result = 1;
+
 		for (int i = 1; i <= power; i++) {
 			result = result * base;
 		}
